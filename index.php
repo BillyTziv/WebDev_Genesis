cfbf2c8fad63ad7dacfac2883f106af64ea43861
billytziv <vtzivaras@gmail.com>
2019-10-24 21:45:09 +0300

    Initial Commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>MyProject</title>
+
+    <!-- Link index.php with your CSS -->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    
+    <!-- Link index.php with Google jQuery CDN -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script> 
+
+    <!-- Link index.php with your Javascript -->
+    <script src="js/app.js"></script> 
+</head>
+<body>
+    <h1>IF you see the blue image and this message HTLM and CSS is working just fine.</h1>
+</body>
+</html>
---
+++ js/app.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    console.log("Document is ready!");
+    alert("Jquery and Javascript are working just fine!");
+});
